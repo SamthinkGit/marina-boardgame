b5e6626a7c1ec859b0bcadc80c8adcf83f13ee93
SamthinkGit <sebastianmayorquin@gmail.com>
2024-10-30 14:02:00 +0100

    Built models

--- backend/exercises.py
+++ backend/exercises.py
@@ -1,29 +1,40 @@
 STORIES = [
-"""
-A partir de ahora vas a responder las preguntas que te hagan sobre la siguiente historia:
-```
-Marina y Sergio (pareja) estaban dando una vuelta por un parque del eje cafetero, en lo que Marina
-se distrae un momento para revisar una planta de cafe se aleja de Sergio, lo que hace que
+    """
+Marina y Kekio estaban viviendo juntos, cuando iban a abandonar la casa para
+mudarse a una nueva, escucharon un ruido, Marina intento convencer a Kekio de
+no investigar dentro de la casa, sin embargo el insistio, tuvo una gran sorpresa
+al darse cuenta de que la verdadera Marina estaba encerrada dentro de la casa,
+un espíritu la había sustituido y pretendía escapar haciéndose pasar por ella.
+""",
+    """
+Kekio estaba jugando a Apex en un torneo internacional, tras matar al último
+enemigo, Marina decidió conectar el horno y se fue la luz, esto le hizo acabar
+descalificado de la competición perdiendo un premio de 2 millones de euros.
+""",
+    """
+Marina estaba trabajando en un periódico en su oficina, estaba escribiendo un
+artículo sobre como un narcotraficante mataba a aquellos que informaban sobre el
+o su posición, debido a esto, a los pocos días Marina fue asesinada.
+""",
+    """
+Mientras jugaban al Minecraft, un creeper consiguió spawnear dentro de una
+habitación (nunca entró, apareció dentro) y cuando Marina lo vio, pidió a Sergio
+que cogiera algún arma y lo matase antes de que explotase
+""",
+    """
+Marina y Kekio (pareja) estaban dando una vuelta por un parque del eje cafetero, en lo que Marina
+se distrae un momento para revisar una planta de cafe se aleja de Kekio, lo que hace que
 cuando se de la vuelta ya no lo pueda encontrar. Para su sorpresa el problema y la solución
 de este ejercicio no es que ella se haya perdido, sino que el campo se trata de un parque antiguo
 capaz de tragarse dentro del campo a los viajantes desprevenidos. Para cuando Marina se dio la vuelta,
 ya estaba en las profundidades del eje cafetero.
-```
-Solo puedes responder con Si, No, Irrelevante o especifica la pregunta. Debes ser totalmente sincero.
 """,
-
-"""
-A partir de ahora vas a responder las preguntas que te hagan sobre el siguiente entorno:
-
-```
-Marina y Sergio (pareja) estaban dentro de un manicomio practicamente vacio con los elementos
+    """
+Marina y Kekio (pareja) estaban dentro de un manicomio practicamente vacio con los elementos
 comunes que hay en una sala totalmente cerrada. Estan encerrados por problemas mentales y hay una
 puerta que no puede ser abierta. En esta sala, ademas de otros utiles, hay un hueco, pero no lo vas a decir
 directamente. Si encuentran este hueco, entonces se puede llamar a un gato que hay detras de otra sala y que
 puede traer una llave a traves de ese agujero. El unico modo de saber que hay un gato como mucho podra ser 
 su sonido de llaves colgando.
-```
-Solo puedes responder con Si, No, Irrelevante o especifica la pregunta. Debes ser totalmente sincero.
-
-"""
+""",
 ]
--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,25 @@
 import os
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage, AIMessage
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.pydantic_v1 import BaseModel, Field
+from typing import Literal
+import traceback
+
+
+class Response(BaseModel):
+    answer: Literal["Sí", "No", "Irrelevante", "Especifica la pregunta"] = Field(
+        description="Respuesta"
+    )
+
+
+class Calification(BaseModel):
+    question: str = Field("Haz una pregunta generica sobre el contenido que falta. (e. g. ¿Por que ha pasado X?, "
+                          "¿Donde ha ocurrido?, ¿Quien fue el responsable?). Si no falta nada devuelva Success.")
+    calification: int = Field(
+        description="Numero del 0 al 100 representando el porcentaje de la historia adivinada "
+        "(siendo 0 nada y 100 todo)"
+    )
+
 
 os.environ["OPENAI_API_KEY"] = (
     "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"  # noqa
@@ -10,31 +28,86 @@ os.environ["OPENAI_API_KEY"] = (
 
 class StoryResponder:
 
-    def __init__(self, instructions: str) -> None:
-        llm = ChatOpenAI(model="gpt-4o-mini")
+    def __init__(self) -> None:
+        llm = ChatOpenAI(model="gpt-4o-mini").with_structured_output(Response)
         prompt = ChatPromptTemplate.from_messages(
             [
-                SystemMessage(content=instructions),
-                MessagesPlaceholder("history"),
-                MessagesPlaceholder("input"),
+                (
+                    "system",
+                    """
+                    A partir de ahora vas a responder preguntas sobre una historia que se te va a proveer.
+                    Para cada pregunta, solo puedes reponder con "Si", "No", "Irrelevante" o "Especifica
+                    la pregunta"
+                    La historia sobre la que vas a obtener información es la siguiente:
+                    {story}
+                    """,
+                ),
+                ("human", "{question}"),
             ]
         )
-        self.history: list[BaseMessage] = []
         self.chain = prompt | llm
 
-    def query(self, user_input: str) -> None:
-        response = self.chain.invoke(
-            input={"history": self.history, "input": [HumanMessage(content=user_input)]}
+    def query(self, question: str, story: str) -> None:
+        try:
+            return self.chain.invoke(
+                input={"story": story, "question": question}
+            ).answer
+        except Exception:
+            return (
+                "Ha habido un error al generar la respuesta. Inténtalo de nuevo.\n Error:"
+                + traceback.format_exc()
+            )
+
+
+class Calificator:
+
+    def __init__(self) -> None:
+        llm = ChatOpenAI(model="gpt-4o-mini").with_structured_output(Calification)
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    """
+                    A partir de ahora vas a calificar si una breve descripcion que dara el usuario, contiene
+                    los puntos clave sobre una historia que te voy a dar. Asi pues, si todos los puntos mas importantes
+                    (independientemente de los detalles y palabras usadas) estan presentes en la respuesta del usuario,
+                    le daras la maxima puntuacion, mientras que si dice cosas incorrectas o faltan puntos clave de 
+                    la historia devolveras una calificación baja.
+
+                    La historia es la siguiente:
+                    ```
+                    {story}
+                    ```
+                    """,
+                ),
+                ("human", "{proposal}"),
+            ]
         )
-        self.history.extend(
-            [HumanMessage(content=user_input), AIMessage(content=response.content)]
-        )
-        return response.content
+        self.chain = prompt | llm
+
+    def query(self, proposal: str, story: str) -> None:
+        try:
+            response = self.chain.invoke(
+                input={"story": story, "proposal": proposal}
+            )
+            return response
+        except Exception:
+            return (
+                "Ha habido un error al generar la respuesta. Inténtalo de nuevo.\n Error:"
+                + traceback.format_exc()
+            )
 
 
 if __name__ == "__main__":
-    pass
+    from exercises import STORIES
 
-    # while True:
-    #     question = input("Input: ")
-    #     print(f"AI: {sr.query(question)}")
+    story = STORIES[0]
+    #    sr = StoryResponder()
+    #    while True:
+    #        question = input("Input: ")
+    #        print(f"AI: {sr.query(question=question, story=story)}")
+    calificator = Calificator()
+    while True:
+        proposal = input("Solution: ")
+        response = calificator.query(proposal=proposal, story=story)
+        print(f"AI: {response.calification}: {response.question}")
